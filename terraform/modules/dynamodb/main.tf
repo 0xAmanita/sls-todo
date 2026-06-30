b48e80b8e9f44bc7b4b8214b5dbe9c1b1c0b8c82
0xAmanita <yldeviermagpusao@gmail.com>
2026-07-01 04:09:09 +0800

    fix: add userId atribute for dynamodb gsi

--- terraform/modules/dynamodb/main.tf
+++ terraform/modules/dynamodb/main.tf
@@ -8,10 +8,20 @@ resource "aws_dynamodb_table" "todos" {
     type = "S"
   }
 
+  attribute {
+    name = "userId"
+    type = "S"
+  }
+
   global_secondary_index {
-    name = "UserIdIndex"
-    hash_key = "userId"
+    name            = "UserIdIndex"
     projection_type = "ALL"
+
+    # Index Key Schema
+    key_schema {
+      attribute_name = "userId"
+      key_type           = "HASH"
+    }
   }
 
   tags = var.tags
